f0c703b7a78dc58b4569420e1be1e7292f512c33
jiang.wenqi <jiang.wenqi@navercorp.com>
2019-08-26 17:27:09 +0800

    #

--- pages/index/index.js
+++ pages/index/index.js
@@ -26,7 +26,11 @@ Page({
       houseTime: '1',
       style: '1',
       measure: '1'
-    }
+    },
+    dataList: {},
+    name: '',
+    cardNumber: '',
+    mobile: ''
   },
   onShow () {
     var value = wx.getStorageSync('status')
@@ -79,7 +83,19 @@ Page({
       }
     }, function (err, res) {
       if (res.code == 0) {
+        let result = {}
+        res.DATA.forEach((item) => {
+          if(result.hasOwnProperty(item.companyId)) {
+            result[item.companyId].product.push(item)
+          } else {
+            result[item.companyId] = {}
+            result[item.companyId].companyName = item.companyName
+            result[item.companyId].product = [item]
+          }
+        })
+        console.log(result)
         that.setData({
+          dataList: result,
           step: '1'
         })
         wx.hideLoading()
@@ -103,23 +119,56 @@ Page({
       step: '1'
     })
   },
-  commitOrder () {
-
+  goToCommitOrder (event) {
+    this.setData({
+      companyValue: event.currentTarget.dataset.companyid,
+      productValue: event.currentTarget.dataset.productid,
+      step: '2'
+    })
+  },
+  commitOrder (event) {
+    console.log(this.data)
+  },
+  inputgetName(e) {
+    let name = e.currentTarget.dataset.name;
+    let nameMap = {}
+    if (name.indexOf('.')) {
+      let nameList = name.split('.')
+      if (this.data[nameList[0]]) {
+        nameMap[nameList[0]] = this.data[nameList[0]]
+      } else {
+        nameMap[nameList[0]] = {}
+      }
+      nameMap[nameList[0]][nameList[1]] = e.detail.value
+    } else {
+      nameMap[name] = e.detail.value
+    }
+    this.setData(nameMap)
   },
   onLoad: function () {
     var value = wx.getStorageSync('status')
+    var currentRegisterInfo = app.globalData.currentRegisterInfo
     if(value == 2 || value == '') {
-      console.log('隐藏 index', '|', value, '|')
       wx.switchTab({
         url: '../register/register'
       })
       wx.hideTabBar()
     }else{
-      console.log('显示 index', '|', value, '|')
       this.setData({
         isLogin: true,
-        role: app.globalData.currentRegisterInfo && app.globalData.currentRegisterInfo.role
+        role: currentRegisterInfo && currentRegisterInfo.role
       })
+      if (currentRegisterInfo && currentRegisterInfo.role == 23) {
+        this.setData({
+          operatorId: currentRegisterInfo.userId
+        })
+      } else if (currentRegisterInfo && currentRegisterInfo.role == 24) {
+        this.setData({
+          name: currentRegisterInfo.name,
+          cardNumber: currentRegisterInfo.cardNumber,
+          mobile: currentRegisterInfo.mobile
+        })
+      }
       wx.showTabBar()
     }
   }
