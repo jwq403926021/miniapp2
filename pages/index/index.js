0d76f289315de814199f8970888d7ba30264450e
jiang.wenqi <jiang.wenqi@navercorp.com>
2019-08-27 10:31:17 +0800

    #

--- pages/index/index.js
+++ pages/index/index.js
@@ -120,19 +120,113 @@ Page({
     })
   },
   goToCommitOrder (event) {
+    let that = this
+    util.request({
+      path: '/app/businessdata/company',
+      method: 'GET'
+    }, function (err, res) {
+      let companyValue = res.DATA.findIndex((item) => {return item.id === event.currentTarget.dataset.companyid})
+      let companyList = res.DATA.map(item => { return item.companyName })
+      that.setData({
+        companySourceData: res.DATA,
+        companyValue: companyValue,
+        companyList: companyList
+      })
+    })
+    util.request({
+      path: `/app/businessdata/getProductByCompanyId/${event.currentTarget.dataset.companyid}`,
+      method: 'GET'
+    }, function (err, res) {
+      let productValue = res.DATA.DATA.findIndex((item) => {return item.id === event.currentTarget.dataset.productid})
+      let productList = res.DATA.DATA.map(item => { return item.productName })
+      console.log(productValue, productList)
+      that.setData({
+        productSourceData: res.DATA.DATA,
+        productValue: productValue,
+        productList: productList
+      })
+    })
+    // wx.hideLoading()
     this.setData({
-      companyValue: event.currentTarget.dataset.companyid,
-      productValue: event.currentTarget.dataset.productid,
       step: '2'
     })
   },
+  companyChange (event) {
+    let that = this
+    console.log(this.data.companySourceData[event.detail.value], '##')
+    util.request({
+      path: `/app/businessdata/getProductByCompanyId/${this.data.companySourceData[event.detail.value].id}`,
+      method: 'GET'
+    }, function (err, res) {
+      let productList = res.DATA.DATA.map(item => { return item.productName })
+      that.setData({
+        companyValue: event.detail.value,
+        productSourceData: res.DATA.DATA,
+        productValue: 0,
+        productList: productList
+      })
+    })
+  },
+  productChange (event) {
+    let that = this
+    this.setData({
+      'productValue': event.detail.value
+    })
+  },
   commitOrder (event) {
-    console.log(this.data)
+    let that = this
+    if (!util.checkCardNum(this.data.cardNumber)) {
+      wx.showToast({
+        mask: true,
+        icon: 'none',
+        title: '请输入正确的身份证号',
+        duration: 1000
+      })
+      return false
+    }
+    if (!util.checkPhone(this.data.mobile)) {
+      wx.showToast({
+        mask: true,
+        icon: 'none',
+        title: '请输入正确的手机号',
+        duration: 1000
+      })
+      return false
+    }
+    wx.showLoading({
+      mask: true,
+      title: '提交中'
+    })
+    util.request({
+      path: '/app/businessdata/save',
+      method: 'POST',
+      data: {
+        name: this.data.name,
+        cardNumber: this.data.cardNumber,
+        mobile: this.data.mobile,
+        productId: this.data.productSourceData[this.data.productValue].id,
+        companyId: this.data.companySourceData[this.data.companyValue].id,
+        operatorId: this.data.operatorId
+      }
+    }, function (err, res) {
+      if (res.code == 0 || res.code == 200) {
+        wx.showToast({
+          mask: true,
+          title: '创建成功',
+          icon: 'success',
+          duration: 1000
+        })
+        that.setData({
+          step: '0'
+        })
+        wx.hideLoading()
+      }
+    })
   },
   inputgetName(e) {
     let name = e.currentTarget.dataset.name;
     let nameMap = {}
-    if (name.indexOf('.')) {
+    if (name.indexOf('.') != -1) {
       let nameList = name.split('.')
       if (this.data[nameList[0]]) {
         nameMap[nameList[0]] = this.data[nameList[0]]
